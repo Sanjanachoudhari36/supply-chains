89050895cbffa659e5a62867d0ba864002fe62ff
Sanjana Choudhari <sanjanachoudhari28@gmail.com>
2026-09-17 20:18:41 +0530

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,16 @@
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask! This is the home page.'
+
+@app.route('/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name', 'Guest')
+    return jsonify({'message': f'Hello, {name}!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
